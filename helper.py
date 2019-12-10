37381ec5673dcddf27b3e7a4a25a94f1e856a201
Tobias <40096033+tobytoyin@users.noreply.github.com>
2019-12-10 18:06:44 +0800

    Update helper.py

--- helper.py
+++ helper.py
@@ -12,8 +12,9 @@ def inline_arg(args):
     ------
     list: `[{args_dict1}, {args_dict2}]`
     """
-    condition1 = (args != '') & (args[0:2] == '--')
-    condition2 = args == ''
+    # acceptance criteria
+    condition1 = (args != '') & (args[0:2] == '--') #correct syntax arhs
+    condition2 = args == '' #empty imput
     assert (condition1 | condition2), "Usage: '--func' or '--func(condition, output)' "
     match = re.finditer(
         r'--(?P<func>\w+)(?:\((?P<condition>.*),(?P<output>.*)\))?', args)
