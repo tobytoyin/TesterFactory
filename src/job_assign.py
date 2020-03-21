84653f929a7043361572e39d2f33231c55e73da6
tobias to <tobytoyin1234@gmail.com>
2020-03-21 16:29:57 +0800

    update job assign

--- src/job_assign.py
+++ src/job_assign.py
@@ -1,16 +1,13 @@
-import pandas as pd
-
-
-class Boss:
+class AssignWorkers:
     def __init__(self, test_cases, n_workers):
         """
-        A Boss need to assign workers, check that task required is good. 
+        Assign workers, check that task required is good. 
         Arguments:
         -----
         `test_cases`: df containing all test cases scenario
         `n_workers`: number of workers required.
         """
-        self.test_cases = self._validate_tasks(test_cases)
+        self.test_cases = test_cases
         self.n_workers = self._validate_workers(n_workers)
 
     def _validate_workers(self, n_workers):
@@ -20,15 +17,6 @@ class Boss:
         m = len(self.test_cases)
         # assign the n_workers as total number of cases
         return n_workers if n_workers < m else m
-
-    def _validate_tasks(self, test_cases):
-        assert not test_cases.empty, "test_cases cannot be empty"
-        assert (
-            test_cases.__class__ is pd.DataFrame
-        ), "test_cases needs to be a DataFrame"
-
-        # test_cases['result'] = None
-        return test_cases
 
     def assign(self):
         """
@@ -42,15 +30,16 @@ class Boss:
 
         ### create worker's task ###
         # calculate number of task per worker
-        task_num = round(len(self.test_cases) / self.n_workers)
+        num_task = round(len(self.test_cases) / self.n_workers)
+        print(num_task)
 
         cur = 0
         for i in range(1, self.n_workers + 1):
             if i == self.n_workers:
                 workers['worker_' + str(i)] = self.test_cases[cur::]
             else:
-                workers['worker_' + str(i)] = self.test_cases[cur : cur + task_num]
-                cur += task_num
+                workers['worker_' + str(i)] = self.test_cases[cur : cur + num_task]
+                cur += num_task
             print(f">> worker_{i} gets a job...")
         return workers
 
